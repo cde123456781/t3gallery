8ec6758f799873aca481146ea1a87ba384653550
cde123456781 <bjc199@uclive.ac.nz>
2026-04-04 23:16:43 +1300

    Scaffold basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Link from "next/link";
 
+
+const mockUrls = [
+  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWfznt6fH5OPRyXvqBu7QnLgbCVrDea9pE5hUxY",
+  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWf2CASeI0ZuM1rWfxOqeYoJ0i28gCFlRVsLG4z",
+  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWfAesBxtJyP0gd7tpXfmwJLqQCGU5eWa129I86",
+  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWf5eyoN2DE3Q0S1eRuZGp8f4B9qPnLVxjUsyYD"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1, 
+  url
+}));
+
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello World
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+      {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+      
+      ))}
+
+      </div>
     </main>
   );
 }
