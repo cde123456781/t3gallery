e36455e41c0abf0ae33becae3a3b66931ec6caa5
cde123456781 <bjc199@uclive.ac.nz>
2026-04-05 01:54:54 +1300

    Setup DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 
 const mockUrls = [
@@ -14,16 +15,23 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-      {mockImages.map((image) => (
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}/>
           </div>
       
-      ))}
+        ))}
 
       </div>
     </main>
