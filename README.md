896a9f4d07f6fba2316a3a4529569bcbb714cd5f
cde123456781 <bjc199@uclive.ac.nz>
2026-04-05 02:24:26 +1300

    Adds images table to the DB and reflects the changes in the page file

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Make it deploy
 - [x] Scaffold basic UI with mock data
 - [x] Tidy up build process
-- [ ] Set up database
-- [ ] Attach database to UI
+- [x] Set up database
+- [x] Attach database to UI
 - [ ] Add authentication
 - [ ] Add image upload
 - [ ] Error management
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,21 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWfznt6fH5OPRyXvqBu7QnLgbCVrDea9pE5hUxY",
-  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWf2CASeI0ZuM1rWfxOqeYoJ0i28gCFlRVsLG4z",
-  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWfAesBxtJyP0gd7tpXfmwJLqQCGU5eWa129I86",
-  "https://eyt0897rgn.ufs.sh/f/2zFACZuM1rWf5eyoN2DE3Q0S1eRuZGp8f4B9qPnLVxjUsyYD"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1, 
-  url
-}));
-
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  console.log(images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url}/>
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
+            <img src={image.url} alt={image.name}/>
+            <div>{image.name}</div>
           </div>
       
         ))}
